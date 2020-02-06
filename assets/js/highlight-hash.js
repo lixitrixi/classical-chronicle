9b8f5c59a0b29f92c98720ff298210557e9f9ace
AlpacaFur <luke@bigring.org>
2020-02-06 18:39:42 -0500

    Add offset to contributor scoll for mobile

--- assets/js/highlight-hash.js
+++ assets/js/highlight-hash.js
@@ -1,2 +1,2 @@
-let highlightedElem = document.getElementById(window.location.hash.slice(1));
+let highlightedElem = document.getElementById(window.location.hash.slice(1)+"-elem");
 if (highlightedElem) highlightedElem.classList.add("highlight")
