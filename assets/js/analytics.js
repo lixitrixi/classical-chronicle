cf662927a9cb869ba8c3758c06449cd3d526c75a
AlpacaFur <luke@bigring.org>
2020-04-06 22:19:35 -0400

    Remove hash from analytics tracking

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -1,5 +1,5 @@
 // This just increments a visitor counter for the page, no other data is transmitted.
 fetch("/analytics/increment_user", {
   "method": "POST",
-  "body": window.location.pathname + window.location.hash
+  "body": window.location.pathname
 })
