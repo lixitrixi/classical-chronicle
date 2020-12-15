e37b5cd07dc9a1bbdcc1399fa1c2ca906d63d2c7
Luke Taylor <luke@bigring.org>
2020-12-14 22:26:39 -0500

    Add favicon to cache list

--- schedule/service-worker.js
+++ schedule/service-worker.js
@@ -3,7 +3,8 @@ let contentToCache = [
   "/schedule/index.js",
   "/schedule/index.css",
   "/schedule/",
-  "/schedule/manifest.webmanifest"
+  "/schedule/manifest.webmanifest",
+  "favicon.ico"
 ]
 let cacheName = "schedule-cache-v1"
 let contentCache;
@@ -11,7 +12,7 @@ let contentCache;
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
     self.skipWaiting();
-    
+
     e.waitUntil(
       caches.open(cacheName).then((cache) => {
         console.log('[Service Worker] Caching Schedule Files');
