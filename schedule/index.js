2c08487229cd2564f8cc28570e8833d90c8b85e3
Luke Taylor <luke@bigring.org>
2021-01-26 23:40:13 -0500

    Add overrides for wednesday and thursday

--- schedule/index.js
+++ schedule/index.js
@@ -620,8 +620,8 @@ function init() {
 
 const scheduleOverrides = {
   // "2021-01-19": demo,
-  "2021-01-20": wednesday,
-  "2021-01-21": regular
+  "2021-01-27": regular,
+  "2021-01-28": regular
 }
 
 init()
